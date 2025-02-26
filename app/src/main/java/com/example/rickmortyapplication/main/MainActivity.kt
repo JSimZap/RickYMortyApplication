e8b4627e19d181d9fb4aa09855d4cb3e99eb79b9
JSimZap <javiorce@gmail.com>
2025-02-26 17:06:03 +0100

    Subida Proyecto Android Sin Terminar RickyMorty 26/02/2025

--- app/src/main/java/com/example/rickmortyapplication/main/MainActivity.kt
+++ app/src/main/java/com/example/rickmortyapplication/main/MainActivity.kt
@@ -2,7 +2,6 @@ package com.example.rickmortyapplication.main
 
 import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import android.widget.ImageButton
 import androidx.appcompat.app.AppCompatActivity
 import com.example.rickmortyapplication.R
