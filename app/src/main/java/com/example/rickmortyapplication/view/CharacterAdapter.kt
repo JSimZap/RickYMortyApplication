bd56e39eb71417275c97259474504eb763d0acb4
JSimZap <javiorce@gmail.com>
2025-02-26 01:44:49 +0100

    Subida Proyecto Android Sin Terminar RickyMorty 25/02/2025

---
+++ app/src/main/java/com/example/rickmortyapplication/view/CharacterAdapter.kt
@@ -0,0 +1,54 @@
+package com.example.rickmortyapplication.view
+
+import android.annotation.SuppressLint
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.rickmortyapplication.R
+import com.example.rickmortyapplication.model.Personaje
+import com.squareup.picasso.Picasso
+
+class CharactersAdapter(
+    private var characters: List<Personaje>,
+    private val onItemClick: (Personaje) -> Unit
+) : RecyclerView.Adapter<CharactersAdapter.CharacterViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CharacterViewHolder {
+        val view =
+            LayoutInflater.from(parent.context).inflate(R.layout.item_character, parent, false)
+        return CharacterViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: CharacterViewHolder, position: Int) {
+        val character = characters[position]
+        holder.characterName.text = character.name
+        Picasso.get().load(character.image).into(holder.characterImage)
+        holder.itemView.setOnClickListener {
+            onItemClick(character)
+        }
+    }
+
+    override fun getItemCount(): Int = characters.size
+
+
+    @SuppressLint("NotifyDataSetChanged")
+    fun updateCharacters(newCharacters: List<Personaje>) {
+        characters = newCharacters
+        notifyDataSetChanged()
+    }
+
+    inner class CharacterViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val characterImage: ImageView = itemView.findViewById(R.id.characterImage)
+        val characterName: TextView = itemView.findViewById(R.id.characterName)
+
+        fun bind(character: Personaje) {
+            characterName.text = character.name
+
+            Picasso.get().load(character.image).into(characterImage)
+
+        }
+    }
+}
---
+++ app/src/main/java/com/example/rickmortyapplication/view/EpisodesActivity.kt
@@ -0,0 +1,97 @@
+package com.example.rickmortyapplication.view
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.Spinner
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.rickmortyapplication.R
+import com.example.rickmortyapplication.model.Episodio
+import com.example.rickmortyapplication.model.RetrofitInstance
+import kotlinx.coroutines.launch
+
+class EpisodesActivity : AppCompatActivity() {
+    private lateinit var temporadaAdapter: ArrayAdapter<String>
+    private val listaEpisodios = mutableListOf<Episodio>()
+    private lateinit var episodioAdapter: EpisodesAdapter
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_episodes)
+
+
+        val spinnerSeasons: Spinner = findViewById(R.id.spinnerSeasons)
+        val episodesRecyclerView: RecyclerView = findViewById(R.id.episodesList)
+
+
+        episodioAdapter = EpisodesAdapter(listaEpisodios)
+        episodesRecyclerView.layoutManager = LinearLayoutManager(this)
+        episodesRecyclerView.adapter = episodioAdapter
+
+
+        obtenerEpisodios(1)
+
+
+        spinnerSeasons.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(
+                parent: AdapterView<*>,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                val selectedSeason = temporadaAdapter.getItem(position).toString()
+                mostrarEpisodios(selectedSeason)
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>) {}
+        }
+        findViewById<Button>(R.id.btnVolver).setOnClickListener {
+            finish()
+        }
+    }
+
+
+    @SuppressLint("NotifyDataSetChanged")
+    private fun obtenerEpisodios(page: Int) {
+        lifecycleScope.launch {
+            try {
+                val response = RetrofitInstance.api.getEpisodes(page)
+                listaEpisodios.addAll(response.resultado)
+                episodioAdapter.notifyDataSetChanged()
+
+                // Caragamos por si hay mas paginas
+                response.info.next?.let {
+                    val nextPage = page + 1
+                    obtenerEpisodios(nextPage)
+                } ?: run {
+
+                    //Filtramos las temporadas
+                    val seasons = listaEpisodios.map { it.episode.substring(0, 4) }.distinct()
+                    temporadaAdapter = ArrayAdapter(
+                        this@EpisodesActivity,
+                        android.R.layout.simple_spinner_item,
+                        seasons
+                    )
+                    temporadaAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+                    findViewById<Spinner>(R.id.spinnerSeasons).adapter = temporadaAdapter
+                }
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
+    }
+
+
+    private fun mostrarEpisodios(season: String) {
+        val filteredEpisodes = listaEpisodios.filter { it.episode.startsWith(season) }
+        episodioAdapter.updateEpisodes(filteredEpisodes)
+    }
+}
+
+
